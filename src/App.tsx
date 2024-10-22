8bbda25b0e476d161ccf252fb0748ed158cfd998
Jeffrey V <luanvecino@hotmail.com>
2024-10-21 22:35:39 -0500

    Entregable_01

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,36 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Cabecera from './components/Cabecera';
 import Inicio from './components/Inicio';
-// Otros componentes...
+import RegistrarArtistas from './components/RegistrarArtistas';
+import ListarArtistas from './components/ListarArtistas';
+import AdministrarArtistas from './components/AdministrarArtistas';
+import Acerca from './components/Acerca';
+import PaginaNoEncontrada from './components/PaginaNoEncontrada';
 
 const App: React.FC = () => {
   return (
     <Router>
       <Cabecera />
-      <Switch>
-        <Route path="/" exact component={Inicio} />
-        <Route path="/registrar-artistas" component={RegistrarArtistas} />
-        <Route path="/listar-artistas" component={ListarArtistas} />
-        <Route path="/administrar-artistas" component={AdministrarArtistas} />
-        <Route path="/acerca" component={Acerca} />
-        <Route path="*" component={PaginaNoEncontrada} />
-      </Switch>
+      <Routes>
+        {/* Ruta para la página de inicio */}
+        <Route path="/" element={<Inicio />} />
+        
+        {/* Ruta para registrar artistas */}
+        <Route path="/registrar-artistas" element={<RegistrarArtistas />} />
+        
+        {/* Ruta para listar artistas */}
+        <Route path="/listar-artistas" element={<ListarArtistas />} />
+        
+        {/* Ruta para administrar artistas */}
+        <Route path="/administrar-artistas" element={<AdministrarArtistas />} />
+        
+        {/* Ruta para la página "Acerca de" */}
+        <Route path="/acerca" element={<Acerca />} />
+        
+        {/* Ruta para manejar páginas no encontradas */}
+        <Route path="*" element={<PaginaNoEncontrada />} />
+      </Routes>
     </Router>
   );
 };
